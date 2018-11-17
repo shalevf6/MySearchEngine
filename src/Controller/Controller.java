31027ca480513897f0fd535debdb66eb58a53fbb
shalevf6 <38422152+shalevf6@users.noreply.github.com>
2018-11-17 20:13:19 +0200

    Updated ReadFile and Parse threads in Controller class Updated Parse and Readfile builders.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,12 +1,13 @@
 package Controller;
 
+import Part_1.Parse;
+import Part_1.ReadFile;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.CheckBox;
 import javafx.stage.DirectoryChooser;
-
 import java.io.File;
 
 public class Controller {
@@ -15,6 +16,7 @@ public class Controller {
     public TextField corpusPath;
     public CheckBox stemmingCheckBox;
     public ChoiceBox<String> languageChoiceBox;
+    public static boolean stemming = true;
 
     public void onCorpusBrowse(ActionEvent actionEvent) {
         DirectoryChooser directoryChooser = new DirectoryChooser();
@@ -31,17 +33,33 @@ public class Controller {
     }
 
     public void onActivate(ActionEvent actionEvent) {
-        boolean stemming = stemmingCheckBox.isSelected();
+        stemming = stemmingCheckBox.isSelected();
         if (postingPath.getText().equals("") || corpusPath.getText().equals("")) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setContentText("You must fill all necessary paths!");
             alert.show();
         }
         else {
-            if (stemming) {
-
-            } else {
-
+            String dirPath = corpusPath.getText();
+            File stopWords = new File(dirPath + "\\stop words");
+            if (stopWords.exists()) {
+                Parse parse = new Parse(dirPath + "\\stop words");
+                ReadFile readFile = new ReadFile(dirPath);
+                Thread readFileThread = new Thread(readFile);
+                Thread parseThread = new Thread(parse);
+                parseThread.start();
+                readFileThread.start();
+                try {
+                    readFileThread.join();
+                    parseThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("You must choose an existing stop words file path!");
+                alert.show();
             }
         }
     }
--- src/Part_1/ReadFile.java
+++ src/Part_1/ReadFile.java
@@ -1,37 +1,54 @@
 package Part_1;
 
+import javafx.scene.control.Alert;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.*;
 import java.nio.file.Files;
-import java.util.ArrayList;
 
-public class ReadFile {
+public class ReadFile implements Runnable {
 
-    public void readThroughFiles(String dirPath) throws FileNotFoundException {
+    private String dirPath;
+    public ReadFile(String dirPath) {
+        this.dirPath = dirPath;
+    }
+
+    public void readThroughFiles() {
         File dir = new File(dirPath + "\\corpus");
-        File[] subDirs = dir.listFiles();
-        if (subDirs != null) {
-            for (File f : subDirs) {
-                File[] tempFiles = f.listFiles();
-                if (tempFiles != null) {
-                    try {
-                        Document document = Jsoup.parse(new String(Files.readAllBytes(f.toPath())));
-                        Elements elements = document.getElementsByTag("DOC");
-                        GeneralClasses.Document doc = new GeneralClasses.Document(elements.text());
-                        for (Element element : elements) {
-                            String text = element.getElementsByTag("TEXT").text();
-                            doc.addDocText(text);
+        if (dir.exists()) {
+            File[] subDirs = dir.listFiles();
+            if (subDirs != null) {
+                for (File f : subDirs) {
+                    File[] tempFiles = f.listFiles();
+                    if (tempFiles != null) {
+                        try {
+                            Document document = Jsoup.parse(new String(Files.readAllBytes(f.toPath())));
+                            Elements elements = document.getElementsByTag("DOC");
+                            GeneralClasses.Document doc = new GeneralClasses.Document(elements.text());
+                            for (Element element : elements) {
+                                String text = element.getElementsByTag("TEXT").text();
+                                doc.addDocText(text);
+                            }
+                            // Parse.docQueue.add(doc);
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                        // Parse.docQueue.add(doc);
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
                 }
             }
+            Parse.stop();
         }
-        Parse.stop();
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("You must choose an existing corpus folder path!");
+            alert.show();
+        }
+    }
+
+    @Override
+    public void run() {
+        readThroughFiles();
     }
 }
