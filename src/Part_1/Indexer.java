e45de60139f13931161baf9ada1bfa0d9f27234e
shalevf6 <38422152+shalevf6@users.noreply.github.com>
2018-11-17 19:41:29 +0200

    Added GUI elements. Added Indexer queue. Changed ReadFile. Updated Document class.

--- src/Part_1/Indexer.java
+++ src/Part_1/Indexer.java
@@ -1,4 +1,24 @@
 package Part_1;
 
+import java.util.Queue;
+
 public class Indexer {
+
+    static protected Queue<String> docQueue;
+    static protected boolean stop;
+
+    public static void stop() {
+        stop = true;
+    }
+
+    public void parseAll(){
+        while (true) {
+            if (!docQueue.isEmpty()) {
+
+            }
+            if (stop) {
+                break;
+            }
+        }
+    }
 }
