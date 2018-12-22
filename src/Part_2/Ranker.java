37501ff5c0b1e347f02fc90febc22c783db982a3
idsh <idsh@post.bgu.ac.il>
2018-12-22 22:16:25 +0200

    Ranker update number 2

--- src/Part_2/Ranker.java
+++ src/Part_2/Ranker.java
@@ -62,4 +62,38 @@ public class Ranker {
         });
         return rankToreturn;
     }
+
+    /**function that returns the rank of a specific term in a specific document according to the query(BM25).
+     * @param tf  term frequency
+     * @param numOfDocs number of documents
+     * @param docLength document length
+     * @param avgDocLength average all document length
+     * @param docFrequency number of documents contains Q_i
+     * @return idf
+     */
+    public double getRankIdf(double tf, double numOfDocs, double docLength, double avgDocLength, double docFrequency){
+         double k_1 = 1.2;
+         double b = 0.75;
+         double k = k_1 * ( (  1 - b ) + ( ( b * docLength ) / avgDocLength ) );
+         double weight = ( ( ( k_1 + 1 ) * tf ) / (k + tf ) ) ;
+         //calculate IDF//
+         double idf = weight * Math.log ( ( numOfDocs - docFrequency + 0.5) / ( docFrequency + 0.5));
+        return idf;
+    }
+
+    /**returns the whole Query score by in a specific document.
+     * @param tf term frequency
+     * @param numOfDocs number of documents in the corpus
+     * @param docLength the length of the document
+     * @param avgDocLength average document length in the corpus
+     * @param docFrequency number of documents contains Q_i
+     * @return
+     */
+    public double getScore( double[]  tf, double numOfDocs,double[] docLength, double avgDocLength, double[] docFrequency){
+        double ans = 0.0;
+        for(int i = 0 ; i < tf.length ; i++){
+            ans = ans + getRankIdf(tf[i], numOfDocs, docLength[i], avgDocLength,docFrequency[i]  );
+        }
+        return ans;
+    }
 }
